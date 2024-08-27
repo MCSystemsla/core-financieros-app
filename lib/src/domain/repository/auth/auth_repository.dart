c58df9dac8f456f3c5578db3ee6fce94a72ef2b4
Jesus Reyes <programador8@mcsystems.la>
2024-08-27 15:59:53 -0600

    Feat: Auth Login Endpoints implemented

--- lib/src/domain/repository/auth/auth_repository.dart
+++ lib/src/domain/repository/auth/auth_repository.dart
@@ -1,13 +1,14 @@
 import 'package:core_financiero_app/global_locator.dart';
 import 'package:core_financiero_app/src/api/api_repository.dart';
 import 'package:core_financiero_app/src/domain/entities/responses/branch_team_response.dart';
+import 'package:core_financiero_app/src/domain/exceptions/app_exception.dart';
 import 'package:core_financiero_app/src/domain/repository/auth/endpoint/auth_endpoint.dart';
 
 abstract class AuthRepository {
-  Future<void> login({
-    required String user,
+  Future<Map<String, dynamic>> login({
+    required String userName,
     required String password,
-    required String branchTeam,
+    required String dbName,
   });
   Future<BranchTeamResponse> getBranchTeam({required String accessCode});
 }
@@ -16,12 +17,22 @@ class AuthRepositoryImpl extends AuthRepository {
   final _api = global<APIRepository>();
   // final _logger = Logger();
   @override
-  Future<void> login({
-    required String user,
+  Future<Map<String, dynamic>> login({
+    required String userName,
     required String password,
-    required String branchTeam,
-  }) {
-    throw UnimplementedError();
+    required String dbName,
+  }) async {
+    final endpoint = LoginEndpoint(
+      userName: userName,
+      password: password,
+      dbName: dbName,
+    );
+    try {
+      final resp = await _api.request(endpoint: endpoint);
+      return resp;
+    } catch (e) {
+      throw AppException.toAppException(e);
+    }
   }
 
   @override
--- lib/src/domain/repository/auth/endpoint/auth_endpoint.dart
+++ lib/src/domain/repository/auth/endpoint/auth_endpoint.dart
@@ -10,7 +10,6 @@ class BranchTeamEndpoint extends Endpoint {
   Method get method => Method.get;
 
   @override
-  // String get path => '/sucursales/obtener-sucursales';
   String get path => '/sucursales/obtener-sucursales-movil';
   // @override
   // Map<String, String> get headers => {
@@ -25,4 +24,29 @@ class BranchTeamEndpoint extends Endpoint {
   // Map<String, dynamic> get body => {
   //       // if (bodyMap != null) ...bodyMap!,
   //     };
+}
+
+class LoginEndpoint extends Endpoint {
+  final String userName;
+  final String password;
+  final String dbName;
+  LoginEndpoint({
+    required this.userName,
+    required this.password,
+    required this.dbName,
+  });
+
+  @override
+  Method get method => Method.post;
+
+  @override
+  String get path => '/auth/login';
+
+  @override
+  Map<String, dynamic> get body => {
+        // if (bodyMap != null) ...bodyMap!,
+        'username': userName,
+        'password': password,
+        'database': dbName
+      };
 }
