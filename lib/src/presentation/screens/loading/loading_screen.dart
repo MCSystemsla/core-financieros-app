46ab1482828134db08017a4ed6ede49548e09b66
Jesus Reyes <programador8@mcsystems.la>
2024-11-15 15:12:29 -0600

    Fixes: Validate perform internet connection

--- lib/src/presentation/screens/loading/loading_screen.dart
+++ lib/src/presentation/screens/loading/loading_screen.dart
@@ -1,13 +1,22 @@
+import 'package:core_financiero_app/src/presentation/bloc/internet_connection/internet_connection_cubit.dart';
+import 'package:core_financiero_app/src/presentation/screens/auth/login/login_screen.dart';
+import 'package:core_financiero_app/src/presentation/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoadingScreen extends StatelessWidget {
   const LoadingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
+    return Scaffold(
+      body: BlocBuilder<InternetConnectionCubit, InternetConnectionState>(
+        builder: (context, state) {
+          return switch (state.isConnected) {
+            true => const LoginScreen(),
+            false => const HomeScreen(),
+          };
+        },
       ),
     );
   }
