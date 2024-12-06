1604e199fca3808c70a528b9138df8fb03ea1bb5
Jesus Reyes <programador8@mcsystems.la>
2024-12-06 16:57:21 -0600

    .

--- lib/src/presentation/screens/cartera/kiva/offline_form_screen.dart
+++ lib/src/presentation/screens/cartera/kiva/offline_form_screen.dart
@@ -7,7 +7,6 @@ import 'package:core_financiero_app/src/presentation/widgets/search_bar/search_b
 import 'package:core_financiero_app/src/utils/extensions/date/date_extension.dart';
 import 'package:core_financiero_app/src/utils/extensions/string/string_extension.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
