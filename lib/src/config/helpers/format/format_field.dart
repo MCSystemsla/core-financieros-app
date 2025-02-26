5442964e9b4eebdd95450a8400c1c9d42dc1cf76
Jesus Reyes <programador8@mcsystems.la>
2025-02-26 11:02:15 -0600

    Feat: Formatted number monto

---
+++ lib/src/config/helpers/format/format_field.dart
@@ -0,0 +1,15 @@
+import 'package:intl/intl.dart';
+
+class FormatField {
+  static String formatMonto(String value) {
+    if (value.isEmpty) return value;
+
+    String newValue = value.replaceAll(',', '');
+
+    int? number = int.tryParse(newValue);
+    if (number == null) return value;
+
+    final NumberFormat formatter = NumberFormat('#,##0.00');
+    return formatter.format(number);
+  }
+}
