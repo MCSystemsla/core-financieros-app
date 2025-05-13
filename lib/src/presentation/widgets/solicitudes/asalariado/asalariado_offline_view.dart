464cbf0ab6a00cff469958667293a2bc274e5062
Jesus Reyes <programador8@mcsystems.la>
2025-05-13 17:18:21 -0600

    .

--- lib/src/presentation/widgets/solicitudes/asalariado/asalariado_offline_view.dart
+++ lib/src/presentation/widgets/solicitudes/asalariado/asalariado_offline_view.dart
@@ -10,7 +10,7 @@ class AsalariadoOfflineView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Trabajar en asalariado offline
+    /// TODO: Trabajar en asalariado offline
     return const Placeholder();
   }
 }
