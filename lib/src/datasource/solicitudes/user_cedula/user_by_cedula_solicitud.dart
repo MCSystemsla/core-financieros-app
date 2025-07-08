251e90c7333e4c6aef96a8eae2c95ef8ba69c0bf
Jesus Reyes <luisdejesus200122@gmail.com>
2025-07-08 17:45:58 -0600

    refactor

---
+++ lib/src/datasource/solicitudes/user_cedula/user_by_cedula_solicitud.dart
@@ -0,0 +1,31 @@
+import 'package:core_financiero_app/src/presentation/widgets/shared/dropdown/jlux_dropdown.dart';
+
+class UserByCedulaSolicitud {
+  final String? primerNombre;
+  final String? segundoNombre;
+  final String? primerApellido;
+  final String? segundoApellido;
+  final String cedula;
+  final DateTime? fechaEmision;
+  final DateTime? fechaVencimiento;
+  final DateTime? fechaNacimiento;
+  final String tipoDocumento;
+  final String? tipoPersona;
+  final Item? paisEmisor;
+  final String? sexo;
+
+  UserByCedulaSolicitud({
+    this.primerNombre,
+    this.segundoNombre,
+    this.primerApellido,
+    this.segundoApellido,
+    required this.cedula,
+    this.fechaEmision,
+    this.fechaVencimiento,
+    this.fechaNacimiento,
+    required this.tipoDocumento,
+    this.paisEmisor,
+    this.tipoPersona,
+    this.sexo,
+  });
+}
